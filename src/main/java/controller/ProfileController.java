892b4fa7139ee7a7051d632b2b77d38f87592f94
Michaelk003 <s3944778@student.rmit.edu.au>
2024-06-03 15:16:30 +1000

    View Profile shows data

--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Model;
@@ -12,6 +13,37 @@ public class ProfileController {
     private Stage parentStage;
 
 
+    @FXML
+    private Label username;
+    @FXML
+    private Label password;
+    @FXML
+    private Label firstname;
+    @FXML
+    private Label lastname;
+    @FXML
+    private Label vip;
+
+
+    @FXML
+    public void initialize() {
+        setData();
+    }
+
+    public void setData() {
+        // Add your code here to set the data in the profile view
+        username.setText( model.getCurrentUser().getUsername());
+        password.setText( model.getCurrentUser().getPassword());
+        firstname.setText( model.getCurrentUser().getFirstname());
+        lastname.setText( model.getCurrentUser().getLastname());
+
+        if (model.getCurrentUser().getVip() == false) {
+            vip.setText("No");
+        } else {
+            vip.setText("Yes");
+        }
+
+    }
 
 
 
